b029025779af2897d0db5b8a82bad4dbc1a54ee2
luis <luic.sistem@gmail.com>
2020-02-06 09:56:21 -0500

    limpiando el formulario reset

--- app.js
+++ app.js
@@ -10,9 +10,29 @@ class Product{
 
 
 class Ui{
-    addProduct(){
+    addProduct(product){
+      const productList =  document.getElementById('product-list');
+      const element = document.createElement('div');
+      element.innerHTML = ` 
+      <div class="card text-center mb-4 ">
+      <div class="card-body">
+          <strong>Product Name </strong>:   ${product.name}
+          <strong>Product Price </strong>:  ${product.price}
+          <strong>Product brand </strong>:  ${product.brand}
+
+      </div>
+  </div>
+ 
+      `;
+ productList.appendChild(element);
 
     }
+    resetForm(){
+        document.getElementById('product-form').reset();
+
+    }
+
+
     addDelete(){
 
     }
@@ -27,9 +47,17 @@ class Ui{
    const name = document.getElementById('name').value;
    const price = document.getElementById('price').value;
    const brand = document.getElementById('brand').value;
+
+   const product = new Product(name, price , brand);
+
+   const ui = new Ui();
+   ui.addProduct(product);
+   ui.resetForm();
+
+
    e.preventDefault()
 
-
+ 
      
  } )
 
