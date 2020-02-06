55e53cd2a2cdb372bea3c07771a130bfc320d59c
luis <luic.sistem@gmail.com>
2020-02-06 08:05:21 -0500

    capturando el evento submit

---
+++ app.js
@@ -0,0 +1,31 @@
+class Product{
+    constructor(name, price, brand){
+        this.name = name;
+        this.price = price;
+        this.brand = brand;
+
+    }
+   
+}
+
+
+class Ui{
+    addProduct(){
+
+    }
+    addDelete(){
+
+    }
+    addMessage(){
+
+    }
+}
+
+// Dom Events
+ document.getElementById('product-form')
+  .addEventListener('submit', function(){
+alert('buenos dias alegria')
+     
+ } )
+
+ 
